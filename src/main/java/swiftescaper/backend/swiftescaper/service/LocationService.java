24d2a06f2aa35f804193e6c6381e69a93d9a1e40
Sihun23 <sihun23@gmail.com>
2024-08-09 18:31:49 +0900

    Feat: LocationService 수정-tunnel,user 객체변환

--- src/main/java/swiftescaper/backend/swiftescaper/service/LocationService.java
+++ src/main/java/swiftescaper/backend/swiftescaper/service/LocationService.java
@@ -6,21 +6,38 @@ import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import swiftescaper.backend.swiftescaper.domain.Location;
+import swiftescaper.backend.swiftescaper.domain.User;
+import swiftescaper.backend.swiftescaper.domain.Tunnel;
 import swiftescaper.backend.swiftescaper.repository.LocationRepository;
+import swiftescaper.backend.swiftescaper.repository.UserRepository;
+import swiftescaper.backend.swiftescaper.repository.TunnelRepository;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
+
     private final LocationRepository locationRepository;
+    private final UserRepository userRepository;
+    private final TunnelRepository tunnelRepository;
+
     public void sendLocation(Double lat, Double lng, Long tunnelId, Long userId) {
+
+        // userId로 User 가져오기
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+
+        // tunnelId로 Tunnel 가져오기
+        Tunnel tunnel = tunnelRepository.findById(tunnelId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid tunnel ID: " + tunnelId));
+
         Location location = Location.builder()
                 .lat(lat)
                 .lng(lng)
-                .tunnelId(tunnelId)
-                .userId(userId)
+                .tunnel(tunnel)
+                .user(user)
                 .build();
 
+        // Location 저장
         locationRepository.save(location);
-        return;
     }
 }
